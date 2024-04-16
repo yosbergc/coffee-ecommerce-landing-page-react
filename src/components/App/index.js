cce3b65a3052c749b2e07a60f57714830ba7d855
Yosber Guerrero <yosbergc@gmail.com>
2024-04-16 17:05:57 -0400

    Some design style fixed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -27,7 +27,7 @@ function App() {
       </header>
       <section className='collection-container'>
         <h1>Our Collection</h1>
-        <p>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+        <p className='mainP'>Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
         <section className='button-section'>
           <ButtonComponent isActiveNumber={0} changeCurrentActive={changeCurrentActive} currentActive={currentActive} text={"All Products"}/>
           <ButtonComponent isActiveNumber={1} changeCurrentActive={changeCurrentActive} currentActive={currentActive} text={"Available Now"}/>
